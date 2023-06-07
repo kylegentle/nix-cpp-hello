272e359703ec6a89bdca3fea9333f622a199ab7c
Kyle Gentle <kylegentle@gmail.com>
2023-06-06 21:19:20 -0400

    Remove unused overlay from flake

--- flake.nix
+++ flake.nix
@@ -13,14 +13,9 @@
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
-      # A Nixpkgs overlay.
-      overlay = final: prev: {
-        hello = import ./. { pkgs = prev; };
-      };
-
       packages = forAllSystems (system:
         {
           default = import ./default.nix { pkgs = nixpkgsFor.${system}; };
